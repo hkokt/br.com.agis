d104a4947516e0ff26d5c621ae5acab7254f7822
Frm2003 <felipper475@gmail.com>
2024-03-06 19:41:10 -0300

    correcoes

---
+++ src/components/utils.js
@@ -0,0 +1,15 @@
+const server = "https://api-agis.onrender.com/";
+//http://localhost:8080
+
+export default {
+    server: server,
+    alunos: `${server}/alunos`,
+    cursos: `${server}/cursos`,
+    chamadas: `${server}/chamadas`,
+    datas: `${server}/datas`,
+    disciplinas: `${server}/disciplinas`,
+    grades: `${server}/grades`,
+    matriculas: `${server}/matriculas`,
+    professores: `${server}/professores`,
+    turmas: `${server}/turmas`
+}
